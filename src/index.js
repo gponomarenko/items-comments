e3c62585ba78ce2078f119df4fb20a239f278dbf
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-08-30 23:37:45 +0300

    favicon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import Favicon from 'react-favicon';
 import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
+    <Favicon url='https://github.com/gponomarenko/items-comments/tree/main/src/img/favicon.ico' />
     <App />
   </BrowserRouter>
   , document.getElementById('root'),
