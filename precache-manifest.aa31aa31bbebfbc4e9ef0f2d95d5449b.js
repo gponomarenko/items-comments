bc8a5be9f62301372858795cb6753d17740dc155
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-08-30 23:05:21 +0300

    Updates

---
+++ precache-manifest.aa31aa31bbebfbc4e9ef0f2d95d5449b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2691cd57822fc8fd84e8043241ec1227",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74dfeab1c1bbd10e3dd5",
+    "url": "./static/css/2.eacbdd6c.chunk.css"
+  },
+  {
+    "revision": "4a5241c44cf37104f1d9",
+    "url": "./static/css/main.90fb64d7.chunk.css"
+  },
+  {
+    "revision": "74dfeab1c1bbd10e3dd5",
+    "url": "./static/js/2.523af36b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.523af36b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5241c44cf37104f1d9",
+    "url": "./static/js/main.d1267704.chunk.js"
+  },
+  {
+    "revision": "0dd360f4e0e8e9613929",
+    "url": "./static/js/runtime-main.13457d5e.js"
+  }
+]);
